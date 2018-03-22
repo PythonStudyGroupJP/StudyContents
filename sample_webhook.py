576b6ce788106866c8ef4b0636b9214bcb07b17e
Fumi <fantm21@gmail.com>
2018-03-22 23:54:25 +0900

    サンプルコード群
    - sample_app.py
    line-bot-sdk-pythonに載っていたサンプルコードそのまま
    
    - sample_webhook.py
    webhook接続確認を行った時にエラーにしないための対策
    
    - sample_echo.py
    LINEからユーザが送ってきたメッセージをそのままレスポンスとして返す

---
+++ sample_webhook.py
@@ -0,0 +1,57 @@
+#-*- coding:utf-8 -*-
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+
+user_info = {
+    'YOUR_CHANNEL_ACCESS_TOKEN': 'xxx',
+    'YOUR_CHANNEL_SECRET': 'xxx',
+    'host': 'localhost',
+    'port': 8080
+}
+
+app = Flask(__name__)
+line_bot_api = LineBotApi(user_info['YOUR_CHANNEL_ACCESS_TOKEN'])
+handler = WebhookHandler(user_info['YOUR_CHANNEL_SECRET'])
+
+
+@app.route("/", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    # Webhook接続確認か判定
+    if event.reply_token == "00000000000000000000000000000000":
+        return
+    pass
+
+
+if __name__ == "__main__":
+    app.run(
+        host=user_info['host'],
+        port=user_info['port']
+    )
